b8e68360acce848a9f7beb9e974c1441442d02f1
lskywolfll <renealejandrosanchezmorales@gmail.com>
2020-01-07 18:29:26 -0300

    Se termine el crud de las emociones

--- backend/Clases/Emociones.js
+++ backend/Clases/Emociones.js
@@ -31,7 +31,69 @@ class Emociones {
         const conexion = new sql.ConnectionPool(dbconfig);
         const parametros = new sql.Request(conexion);
 
+        parametros.input('NOMBRE', sql.VarChar, this.NOMBRE);
+        parametros.input('VALOR', sql.Int, this.VALOR);
+
         const Procedimiento = 'INSERTAR_EMOCIONES';
+
+        return new Promise((resolve, reject) => {
+            Conexion.ConsultaQuery(Procedimiento, parametros, conexion, (err, respuesta) => {
+                if(err){
+                    reject(err);
+                }else{
+                    resolve(respuesta);
+                }
+            });
+        });
+    }
+
+    Consultar_Por_ID(){
+        const conexion = new sql.ConnectionPool(dbconfig);
+        const parametros = new sql.Request(conexion);
+
+        parametros.input('ID_EMOCION', sql.Int, this.ID_EMOCION);
+
+        const Procedimiento = 'CONSULTAR_EMOCION_POR_ID';
+
+        return new Promise((resolve, reject) => {
+            Conexion.ConsultaQuery(Procedimiento, parametros, conexion, (err, respuesta) => {
+                if(err){
+                    reject(err);
+                }else{
+                    resolve(respuesta);
+                }
+            });
+        });
+    }
+
+    Actualizar_Por_Id(){
+        const conexion = new sql.ConnectionPool(dbconfig);
+        const parametros = new sql.Request(conexion);
+
+        parametros.input('ID_EMOCION', sql.Int, this.ID_EMOCION);
+        parametros.input('NOMBRE', sql.VarChar, this.NOMBRE);
+        parametros.input('VALOR', sql.Int, this.VALOR);
+
+        const Procedimiento = 'ACTUALIZAR_EMOCION_POR_ID';
+
+        return new Promise((resolve, reject) => {
+            Conexion.ConsultaQuery(Procedimiento, parametros, conexion, (err, respuesta) => {
+                if(err){
+                    reject(err);
+                }else{
+                    resolve(respuesta);
+                }
+            });
+        });
+    }
+
+    Eliminar_Por_Id(){
+        const conexion = new sql.ConnectionPool(dbconfig);
+        const parametros = new sql.Request(conexion);
+
+        parametros.input('ID_EMOCION', sql.Int, this.ID_EMOCION);
+
+        const Procedimiento = 'ELIMINAR_EMOCION_POR_ID';
 
         return new Promise((resolve, reject) => {
             Conexion.ConsultaQuery(Procedimiento, parametros, conexion, (err, respuesta) => {
--- backend/Controladores/ControladorEmociones.js
+++ backend/Controladores/ControladorEmociones.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const Emocion = require('../Clases/Emociones');
 const Response = require('../tools/Response');
+const _ = require('underscore');
 const RespuestaServicio = require('../Clases/RespuestaServicio');
 
 router.get('/Obtener', (req, res) => {
@@ -23,10 +24,95 @@ router.get('/Obtener', (req, res) => {
         });
 });
 
-router.get('/Obtener/:id', (req, res) => {
+router.get('/Obtener/:id_emocion', (req, res) => {
     const emocion = new Emocion;
+    const objRespuestaServicio = new RespuestaServicio;
 
-    emocion.ID_EMOCION = req.body.ID_EMOCION;
+    emocion.ID_EMOCION = req.params.id_emocion;
+
+    emocion.Consultar_Por_ID()
+        .then(resp => {
+            objRespuestaServicio.Respuesta = 'OK';
+            objRespuestaServicio.Descripcion = resp.recordset;
+
+            Response.success(req, res, objRespuestaServicio, 200);
+        })
+        .catch(err => {
+            objRespuestaServicio.Respuesta = 'NOK';
+            objRespuestaServicio.Descripcion_Error = err;
+
+            Response.error(req, res, objRespuestaServicio, 200, 'Se cae al obtener una emocion por el id');
+        })
+});
+
+router.post('/Crear', (req, res) => {
+    const emocion = new Emocion;
+    const objRespuestaServicio = new RespuestaServicio;
+
+    const data = _.pick(req.body, ['nombre', 'valor']);
+
+    emocion.NOMBRE = data.nombre;
+    emocion.VALOR = data.valor;
+
+    emocion.Crear()
+        .then(resp => {
+            objRespuestaServicio.Respuesta = 'OK';
+            objRespuestaServicio.Descripcion = 'pasa';
+
+            Response.success(req, res, objRespuestaServicio, 200);
+        })
+        .catch(err => {
+            objRespuestaServicio.Respuesta = 'NOK';
+            objRespuestaServicio.Descripcion_Error = err;
+
+            Response.error(req, res, objRespuestaServicio, 200, 'Se cae al crear una emocion');
+        });
+});
+
+router.put('/Actualizar/:id_emocion', (req, res) => {
+    const emocion = new Emocion;
+    const objRespuestaServicio = new RespuestaServicio;
+
+    const data = _.pick(req.body, ['nombre', 'valor']);
+
+    emocion.ID_EMOCION = req.params.id_emocion;
+    emocion.NOMBRE = data.nombre;
+    emocion.VALOR = data.valor;
+
+    emocion.Actualizar_Por_Id()
+        .then(resp => {
+            objRespuestaServicio.Respuesta = 'OK';
+            objRespuestaServicio.Descripcion = resp.recordset;
+
+            Response.success(req,res, objRespuestaServicio, 200);
+        })
+        .catch(err => {
+            objRespuestaServicio.Respuesta = 'NOK';
+            objRespuestaServicio.Descripcion_Error = err;
+
+            Response.error(req, res, objRespuestaServicio, 200, 'Se cae al actualizar una emocion por id');
+        })
+});
+
+router.delete('/Eliminar/:id_emocion', (req, res) => {
+    const emocion = new Emocion;
+    const objRespuestaServicio = new RespuestaServicio;
+
+    emocion.ID_EMOCION = req.params.id_emocion;
+
+    emocion.Eliminar_Por_Id()
+        .then(resp => {
+            objRespuestaServicio.Respuesta = 'OK';
+            objRespuestaServicio.Descripcion = resp.recordset;
+
+            Response.success(req, res, objRespuestaServicio, 200);
+        })
+        .catch(err => {
+            objRespuestaServicio.Respuesta = 'NOK';
+            objRespuestaServicio.Descripcion_Error = err;
+
+            Response.error(req, res, objRespuestaServicio, 200, 'Se cae al eliminar una emocion por id');
+        })
 });
 
 module.exports = router;
