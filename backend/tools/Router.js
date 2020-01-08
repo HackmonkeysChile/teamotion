afa950de41c7fd5dfbd1139a3a83a0846bf1ab8e
lskywolfll <renealejandrosanchezmorales@gmail.com>
2020-01-07 23:22:24 -0300

    Se termina la clase de persona

--- backend/tools/Router.js
+++ backend/tools/Router.js
@@ -1,11 +1,13 @@
 //using de componentes
 // ej: const Demonio = require('../Controladores/Demonio')
 const Emociones = require('../Controladores/ControladorEmociones');
+const Personas = require('../Controladores/ControladorPersonas');
 
 const routes = (server) => {
     // server.use('/nombre', controlador)
     // ej: server.use('/Demonio', Demonio)
     server.use('/Emociones', Emociones);
+    server.use('/Personas', Personas);
 };
 
 module.exports = routes;
